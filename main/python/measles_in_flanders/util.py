938f1e37eb67dcd189e96eb731a4a1b6f40f6919
Elise Kuylen <elise.kuylen@uantwerpen.be>
2018-01-31 14:09:40 +0100

    Added function to create graphs from contact log files.

---
+++ main/python/measles_in_flanders/util.py
@@ -0,0 +1,16 @@
+import networkx as nx
+
+def getSusceptibleContactsGraph(logfile):
+    """
+        Turn logfile of contacts between susceptibles
+        into a graph.
+    """
+    G = nx.Graph()
+    with open(logfile) as f:
+        for line in f:
+            linedata = line.split(' ')
+            p1_id = int(linedata[1])
+            p2_id = int(linedata[2])
+            G.add_nodes_from([p1_id, p2_id])
+            G.add_edge(p1_id, p2_id)
+    return G
