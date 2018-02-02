4b1d6c1d187dedf4ece8f2a44f23c23067d0e8c5
Elise Kuylen <elise.kuylen@uantwerpen.be>
2018-02-02 11:02:33 +0100

    Continued working on python scripts.

--- main/python/measles_in_flanders/baseline.py
+++ main/python/measles_in_flanders/baseline.py
@@ -4,131 +4,61 @@
 
 import itertools
 
-import matplotlib.pyplot as plt
-import numpy as np
-
 import pystride
-from pystride.stride.stride import Id_Household, Id_School, Id_Work, Id_PrimaryCommunity, Id_SecondaryCommunity
 from pystride.Simulation import Simulation
 
-'''
-def countImmune(sim, timestep):
-    if timestep == 0:
-        print("Determining pocket sizes of non-immunized individuals")
-        households = {}
-        schools = {}
-        workplaces = {}
-        communities1 = {}
-        communities2 = {}
+def measureSusceptiblesClustering(sim, timestep):
+    """
+    """
+    if timestep == 9:
+        # Count contacts between susceptibles after 10 timesteps
+        pass
 
-        pop = sim.GetSimulator().GetPopulation()
-        for pIndex in range(pop.size()):
-            person = pop[pIndex]
-            if person.GetHealth().IsSusceptible():
-                addPerson(households, person.GetClusterId(Id_Household), True)
-                addPerson(schools, person.GetClusterId(Id_School), True)
-                addPerson(workplaces, person.GetClusterId(Id_Work), True)
-                addPerson(communities1, person.GetClusterId(Id_PrimaryCommunity), True)
-                addPerson(communities2, person.GetClusterId(Id_SecondaryCommunity), True)
-            else:
-                addPerson(households, person.GetClusterId(Id_Household), False)
-                addPerson(schools, person.GetClusterId(Id_School), False)
-                addPerson(workplaces, person.GetClusterId(Id_Work), False)
-                addPerson(communities1, person.GetClusterId(Id_PrimaryCommunity), False)
-                addPerson(communities2, person.GetClusterId(Id_SecondaryCommunity), False)
+def seedInfection(sim, timestep):
+    """
+    """
+    if timestep == 9:
+        pass
 
-        # Distribution of susceptibles by cluster type'''
-        '''bins = [0.1, 0.2, 0.3, 0.4, 0.5, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-        household_results = toHist(toFractions(households), bins)
-        school_results = toHist(toFractions(schools), bins)
-        work_results = toHist(toFractions(workplaces), bins)
-        comm1_results = toHist(toFractions(communities1), bins)
-        comm2_results = toHist(toFractions(communities2), bins)
+def runBaseline():
+    """
+    """
+    sim = Simulation()
+    # Set parameters
+    sim.runConfig.setParameter("output_prefix", "Baseline")
+    sim.runConfig.setParameter("num_days", 60)
 
-        width = 0.015
+    sim.runConfig.setParameter("disease_config_file", "data/disease_measles.xml")
+    sim.runConfig.setParameter("r0", 11)
+    sim.runConfig.setParameter("seeding_rate", 0)
+    sim.runConfig.setParameter("seeding_age_min", 1)
+    sim.runConfig.setParameter("seeding_age_max", 99)
 
-        fig, ax = plt.subplots()
-        ax.bar(bins, household_results, width=width, color='red')
-        ax.bar([x + width for x in bins], school_results, width, color='tan')
-        ax.bar([x + (2* width) for x in bins], work_results, width, color='lime')
-        ax.bar([x + (3* width) for x in bins], comm1_results, width, color='orange')
-        ax.bar([x + (4* width) for x in bins], comm2_results, width, color='black')
-        plt.show()'''
-        '''
-        # Overall distribution of susceptibles
-        bins = [0.1, 0.2, 0.3, 0.4, 0.5, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-        all_fractions = toFractions(households) + toFractions(schools) + toFractions(workplaces) + toFractions(communities1) + toFractions(communities2)
-        all_results = toHist(all_fractions, bins)
-        plt.ylim([0,1])
-        plt.legend(['Baseline'])
-        plt.bar(bins, all_results, width=0.08)
-        plt.savefig('baseline')'''
+    sim.runConfig.setParameter("population_file", "data/pop_flanders600.csv")
+    sim.runConfig.setParameter("generate_person_file", 0)
+    sim.runConfig.setParameter("num_participants_survey", 10000)
+    sim.runConfig.setParameter("age_contact_matrix_file", "data/contact_matrix_flanders_subpop.xml")
+    sim.runConfig.setParameter("log_level", "SusceptibleContacts")
+    sim.runConfig.setParameter("start_date", "2017-01-01")
+    sim.runConfig.setParameter("holidays_file", "data/holidays_none.json")
+    
+    # immunity_profile
+    # vaccine_profile
+    # vaccine_link_probability
+    # immunity_rate
+    # vaccine_rate
 
-'''
-def toHist(fractions, bins):
-    ys = [0] * len(bins)
-    for frac in fractions:
-        for i_bin in range(len(bins)):
-            if frac <= bins[i_bin]:
-                ys[i_bin] += 1
-                break
-    for i_y in range(len(ys)):
-        ys[i_y] = ys[i_y] / len(fractions)
-    return ys
+    sim.runConfig.setParameter("local_information_policy", "NoLocalInformation")
+    sim.runConfig.setParameter("global_information_policy", "NoGlobalInformation")
+    sim.runConfig.setParameter("belief_policy/name", "NoBelief")
+    sim.runConfig.setParameter("behaviour_policy", "NoBehaviour")
 
+    sim.runConfig.setParameter("rng_seed", 1)
 
-def toFractions(dictionary):
-    fractions = []
-    for key in dictionary:
-        cluster_size = dictionary[key]['susceptible'] + dictionary[key]['other']
-        frac_susceptible = dictionary[key]['susceptible'] / cluster_size
-        fractions.append(frac_susceptible)
-    return fractions
+    # Register callbacks
+    sim.RegisterCallback(measureSusceptiblesClustering)
+    sim.RegisterCallback(seedInfection)
 
-
-def addPerson(dictionary, cluster_id, susceptible):
-    if cluster_id != 0:
-        if not (cluster_id in dictionary):
-            dictionary[cluster_id] = {
-                'susceptible' : 0,
-                'other' : 0
-            }
-
-        if susceptible:
-            dictionary[cluster_id]['susceptible'] += 1
-        else:
-            dictionary[cluster_id]['other'] += 1
-'''
-
-sim = Simulation()
-# Load configuration common to all simulations for this study
-sim.loadRunConfig("config/common.xml")
-
-# Set parameters that are the same for all baseline simulations
-sim.runConfig.setParameter("output_prefix", "Baseline")
-sim.runConfig.setParameter("seeding_rate", 0)
-sim.runConfig.setParameter("local_information_policy", "NoLocalInformation")
-sim.runConfig.setParameter("global_information_policy", "NoGlobalInformation")
-sim.runConfig.setParameter("belief_policy/name", "NoBelief")
-sim.runConfig.setParameter("behaviour_policy", "NoBehaviour")
-
-# TODO have common natural immunity rate?
-# From what age to what age and what percentage?
-
-sim.runConfig.setParameter("num_days", 10)
-
-seeds = [1]
-vaccs = [0.97]
-
-# Create forks with varying seed & vaccination coverage
-for seed, vac_cov in list(itertools.product(seeds, vaccs)):
-    # Create fork
-    fork = sim.fork("vac_cov_" + str(vac_cov) + "_" + str(seed))
-    fork.runConfig.setParameter("rng_seed", seed)
-    fork.runConfig.setParameter("immunity_rate", vac_cov)
-    fork.runConfig.setParameter("vaccine_rate", vac_cov)
-    # Register callback function
-    # fork.registerCallback(countImmune)
-
-# Run all forks
-sim.runForks()
+    # TODO run forks with varying parameters (itertools.product(...))
+    # Run simulation
+    sim.run()
--- main/python/measles_in_flanders/run.py
+++ main/python/measles_in_flanders/run.py
@@ -2,9 +2,12 @@
 #            Running simulations for 'measles in Flanders' study            #
 #############################################################################
 
+from baseline import runBaseline
+
 def main():
     # Run baseline simulations
     print("Running baseline simulations")
+    runBaseline()
     # Run simulations with imitation-based vaccination behaviours
     print("Running simulations with imitation-behaviour")
     # Post-processing
