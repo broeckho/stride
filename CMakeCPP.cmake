300bc7b0a38a53b9ef50b01b7ae3c088a98926e7
Jan Broeckhove <Jan.Broeckhove@uantwerpen.be>
2018-03-19 16:02:15 +0100

    TRNG4.15 —> 4.20 to eliminate problem with fast_discrete_dist.

--- CMakeCPP.cmake
+++ CMakeCPP.cmake
@@ -65,7 +65,7 @@ include_directories(SYSTEM ${CMAKE_HOME_DIRECTORY}/main/resources/lib/spdlog/inc
 #----------------------------------------------------------------------------
 # Tina's Random Number Generator (TRNG) library and paths
 #----------------------------------------------------------------------------
-include_directories(SYSTEM ${CMAKE_HOME_DIRECTORY}/main/resources/lib/trng-4.15/include)
+include_directories(SYSTEM ${CMAKE_HOME_DIRECTORY}/main/resources/lib/trng-4.20/include)
 set(LIBS ${LIBS} trng)
 
 #----------------------------------------------------------------------------
