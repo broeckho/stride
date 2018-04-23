8ce4dedc5e57b37ab1b84e896d0513d4278b7f34
Elise Kuylen <elise.kuylen@uantwerpen.be>
2018-04-23 15:52:54 +0200

    Added Subject and Observer classes for Python environment.

---
+++ main/python/pystride/Subject.py
@@ -0,0 +1,16 @@
+
+class Subject:
+    """
+        Base class for subjects.
+    """
+    def __init__(self):
+        pass
+
+    def registerObserver(self, observer):
+        pass
+
+    def unregisterObserver(self, observer):
+        pass
+
+    def notifyObservers(self, event):
+        pass
