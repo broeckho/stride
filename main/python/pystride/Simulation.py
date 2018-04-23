f246cdda2fb16d54516ddbd688e2599e27d714a7
Lander Willem <lander.willem@uantwerpen.be>
2018-04-23 13:30:35 +0200

    [python simulator] Fixed the dataDir for forks and an issue with data-links if you run the scripts not from "bin".

--- main/python/pystride/Simulation.py
+++ main/python/pystride/Simulation.py
@@ -18,7 +18,7 @@ class Simulation:
             self.dataDir = os.path.join("..", "data")
         else:
             self.dataDir = dataDir
-
+    
     def loadRunConfig(self, filename: str):
         self.runConfig = Config(filename)
         self.diseaseConfig = Config(os.path.join(self.dataDir, self.runConfig.getParameter("disease_config_file")))
@@ -41,7 +41,7 @@ class Simulation:
         return os.path.join(self.getWorkingDirectory(), self.getLabel()+"/")
 
     def _linkData(self):
-        dataDestDir = os.path.join(self.getOutputDirectory(), "data")
+        dataDestDir = os.path.join(self.getWorkingDirectory(), "data_sim")
         os.makedirs(dataDestDir, exist_ok=True)
         file_params = [
             "population_file",
@@ -50,9 +50,9 @@ class Simulation:
             # TODO disease_config_file?
         ]
         for param in file_params:
-            src = os.path.join(self.dataDir, self.runConfig.getParameter(param))
-            self.runConfig.setParameter(param, src)
-            dst = os.path.join(dataDestDir, os.path.basename(src))
+            src = os.path.realpath(os.path.join(self.dataDir,self.runConfig.getParameter(param)))
+            dst = os.path.join(dataDestDir, self.runConfig.getParameter(param))
+            self.runConfig.setParameter(param, dst)
             if (os.path.isfile(src)) and (not (os.path.isfile(dst))):
                 os.symlink(src, dst)
 
@@ -63,7 +63,6 @@ class Simulation:
         """
         if linkData:
             self._linkData()
-
         os.makedirs(self.getOutputDirectory(), exist_ok=True)
         # Store disease configuration
         oldDiseaseFile = self.runConfig.getParameter("disease_config_file")[:-4]
@@ -104,8 +103,10 @@ class Simulation:
             exit(1)
 
     def runForks(self, *args, **kwargs):
+        """ Create the root simulation folder. """
+        os.makedirs(self.getOutputDirectory(), exist_ok=True)
+        
         """ Run all forks but not the root simulation. """
-        self._setup()
         for fork in self.forks:
             fork.run(*args, **kwargs)
 
