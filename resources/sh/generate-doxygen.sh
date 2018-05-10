2696761c5aa14444070bde2a2b309a0be44cf2ee
Jan Broeckhove <Jan.Broeckhove@uantwerpen.be>
2018-05-10 20:50:36 +0200

    Travis check.

--- resources/sh/generate-doxygen.sh
+++ resources/sh/generate-doxygen.sh
@@ -81,6 +81,7 @@ echo 'Generating Doxygen code documentation...'
 cd $TRAVIS_BUILD_DIR && make STRIDE_INCLUDE_DOC=TRUE configure
 cd cmake-build-release/doc/doxygen && make all && mv html $TRAVIS_BUILD_DIR/code_docs
 cd $TRAVIS_BUILD_DIR && mv doc/doxygen/ReferenceManual.html code_docs/index.html
+ls code_docs
 #doxygen $DOXYFILE 2>&1 | tee doxygen.log
 
 exit 0
