e58768f75d9d4a4df66c0f786466968ca59f2edc
Elise Kuylen <elise.kuylen@uantwerpen.be>
2018-05-14 11:10:22 +0200

    Continued working on Python script.

--- main/python/measles_in_flanders/runmeasles.py
+++ main/python/measles_in_flanders/runmeasles.py
@@ -4,11 +4,50 @@
 
 from pystride.PyController import PyController
 
+def runRandom():
+    control = PyController("../config/run_default.xml")
+    # control.runConfig.setParameter("contact_log_level", "Susceptibles")
+    # control.runConfig.setParameter("immunity_profile", "Random" + 35)
+    # control.runConfig.setParameter("immunity_rate", ?)
+    # control.runConfig.setParameter("num_days", 100) ?
+    # control.runConfig.setParameter("num_participants_survey", 10 000)
+    control.runConfig.setParameter("output_prefix", "Random")
+    # vaccine_link_probability
+    # vaccine_profile
+    # vaccine_rate
+    control.control()
+
+def runAgeProfiles():
+    control = PyController("../config/run_default.xml")
+    # control.runConfig.setParameter("contact_log_level", "Susceptibles")
+    # control.runConfig.setParameter("immunity_profile", "Random" + 35)
+    # control.runConfig.setParameter("immunity_rate", ?)
+    # control.runConfig.setParameter("num_days", 100) ?
+    # control.runConfig.setParameter("num_participants_survey", 10 000)
+    control.runConfig.setParameter("output_prefix", "AgeProfiles")
+    # vaccine_link_probability
+    # vaccine_profile
+    # vaccine_rate
+    control.control()
+
+def runClustering():
+    control = PyController("../config/run_default.xml")
+    # control.runConfig.setParameter("contact_log_level", "Susceptibles")
+    # control.runConfig.setParameter("immunity_profile", "Random" + 35)
+    # control.runConfig.setParameter("immunity_rate", ?)
+    # control.runConfig.setParameter("num_days", 100) ?
+    # control.runConfig.setParameter("num_participants_survey", 10 000)
+    control.runConfig.setParameter("output_prefix", "Clustering")
+    # vaccine_link_probability
+    # vaccine_profile
+    # vaccine_rate
+    control.control()
+
 def main():
     print("Running simulations for Measles In Flanders study")
-    # TODO run simulations with random immunity and vaccination
-    # TODO run simulations with age profile for immunity and vaccination
-    # TODO run simulations with age profile and clustering (clustering = 1)
+    runRandom()
+    runAgeProfiles()
+    runClustering()
 
 if __name__=="__main__":
     main()
