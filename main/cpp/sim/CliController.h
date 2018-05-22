e7fd540c119878892323dc4082fd24283f5e2f1e
Jan Broeckhove <Jan.Broeckhove@uantwerpen.be>
2018-05-22 22:56:41 +0200

    Cosmetic.

--- main/cpp/sim/CliController.h
+++ main/cpp/sim/CliController.h
@@ -11,7 +11,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with the software. If not, see <http://www.gnu.org/licenses/>.
  *
- *  Copyright 2017, Kuylen E, Willem L, Broeckhove J
+ *  Copyright 2017, 2018, Kuylen E, Willem L, Broeckhove J
  */
 
 /**
@@ -56,7 +56,7 @@ public:
         void Control();
 
 private:
-        /// Empty controller: used as taget for delegation.
+        /// Empty controller: used as target for delegation.
         explicit CliController();
 
         /// Check install environment.
@@ -77,10 +77,10 @@ private:
 
 private:
         boost::property_tree::ptree     m_config_pt;        ///< Main configuration for run and sim.
-        std::string                     m_output_prefix;    /// Prefix to output (name prefix or prefix dir)
+        std::string                     m_output_prefix;    ///< Prefix to output (name prefix or prefix dir)
         util::Stopwatch<>               m_run_clock;        ///< Stopwatch for timing the computation.
         std::shared_ptr<spdlog::logger> m_stride_logger;    ///< General logger.
-        bool                            m_use_install_dirs; /// Working dir or install dir mode.
+        bool                            m_use_install_dirs; ///< Working dir or install dir mode.
 };
 
 } // namespace stride
