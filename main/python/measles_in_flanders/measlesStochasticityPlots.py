439bb83d54a8de60e364036581778bcfcba90e5e
Elise Kuylen <elise.kuylen@uantwerpen.be>
2018-06-14 13:56:19 +0200

    Small tweaks + boxplots for outbreak sizes.

--- main/python/measles_in_flanders/measlesStochasticityPlots.py
+++ main/python/measles_in_flanders/measlesStochasticityPlots.py
@@ -7,8 +7,10 @@ def main(directory):
     scenarios = ["Random", "AgeDependent", "Clustering"]
     allOutbreaks = []
     allCases = []
+    allOutbreakSizes = []
     for scenarioName in scenarios:
         scenarioOutbreaks = []
+        scenarioOutbreakSizes = []
         scenarioCases = {}
         for day in range(366):
             scenarioCases[day] = []
@@ -24,6 +26,7 @@ def main(directory):
                     scenarioCases[day] += [int(x) for x in row]
                     day += 1
             totalCases = [int(x) for x in cases[-1]] # Get total cases on last sim day
+            scenarioOutbreakSizes += totalCases
             outbreak = [1.0] * len(totalCases)
             for i in range(len(totalCases)):
                 if totalCases[i] <= 1:
@@ -32,14 +35,23 @@ def main(directory):
             pctOutbreaks = sum(outbreak) / len(outbreak)
             scenarioOutbreaks.append(pctOutbreaks)
         allOutbreaks.append(scenarioOutbreaks)
+        allOutbreakSizes.append(scenarioOutbreakSizes)
         allCases.append(scenarioCases)
     plt.boxplot(allOutbreaks, labels=scenarios)
+    plt.ylim((0,1))
     plt.ylabel("% outbreaks per 40 runs")
     plt.show()
+
+    plt.boxplot(allOutbreakSizes, labels=scenarios)
+    plt.ylim((0, 30000))
+    plt.ylabel("Outbreak size")
+    plt.show()
+
     for i in range(len(scenarios)):
         plt.boxplot(list(allCases[i].values())[0::30], labels=list(allCases[i].keys())[0::30])
         plt.xlabel("Day")
         plt.ylabel("Number of infected cases")
+        plt.ylim((0, 30000))
         plt.title("Cases distribution per day for " + scenarios[i])
         plt.show()
 
