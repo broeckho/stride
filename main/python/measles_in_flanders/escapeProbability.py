88d4a0c8de2924e843353c2b7de5a5d39884ba8c
Elise Kuylen <elise.kuylen@uantwerpen.be>
2018-10-24 14:28:59 +0200

    More plots...

--- main/python/measles_in_flanders/escapeProbability.py
+++ main/python/measles_in_flanders/escapeProbability.py
@@ -1,15 +1,7 @@
-'''
 import argparse
 import csv
 import matplotlib.pyplot as plt
 import os
-
-'''
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
-'''
 
 def getRngSeeds(outputDir, scenarioName):
     seeds = []
@@ -30,42 +22,6 @@ def getFinalOutbreakSize(outputDir, scenarioName, seed, numDays):
             if timestep == numDays - 1:
                 return int(row["cases"])
 
-def getFinalOutbreakSizes(outputDir, scenarioName, numDays):
-    finalSizes = []
-    seeds = getRngSeeds(outputDir, scenarioName)
-    for s in seeds:
-        finalSizes.append(getFinalOutbreakSize(outputDir, scenarioName, s, numDays))
-    return finalSizes
-
-def finalSizesPlot(outputDir, scenarioNames, scenarioDisplayNames, numDays, extinctionThreshold):
-    allFinalSizes = []
-    for scenario in scenarioNames:
-        finalSizes = getFinalOutbreakSizes(outputDir, scenario, numDays)
-        finalSizes = [x for x in finalSizes if x > extinctionThreshold]
-        allFinalSizes.append(finalSizes)
-    plt.boxplot(allFinalSizes, labels=scenarioDisplayNames)
-    plt.ylabel("Final outbreak size")
-    plt.savefig("FinalSizesNoExtinction.png")
-    plt.clf()
-
-def outbreakOccurrencePlot(outputDir, scenarioNames, scenarioDisplayNames, numDays, extinctionThreshold):
-    outbreakOccurrences = []
-    for scenario in scenarioNames:
-        finalSizes = getFinalOutbreakSizes(outputDir, scenario, numDays)
-        outbreaks = []
-        for f in finalSizes:
-            if f <= extinctionThreshold:
-                outbreaks.append(0)
-            else:
-                outbreaks.append(1)
-        pctOutbreaks = sum(outbreaks) / len(outbreaks)
-        outbreakOccurrences.append(pctOutbreaks)
-    plt.bar(scenarioDisplayNames, outbreakOccurrences)
-    plt.ylim(0, 1)
-    plt.ylabel("Fraction outbreaks")
-    plt.savefig("OutbreakOccurrencePlot.png")
-    plt.clf()
-
 def getSusceptiblesAtStart(outputDir, scenario, seed):
     totalSusceptibles = 0
     susceptiblesFile = os.path.join(outputDir, scenario + "_" + str(seed), "susceptibles.csv")
@@ -77,25 +33,28 @@ def getSusceptiblesAtStart(outputDir, scenario, seed):
                 totalSusceptibles += 1
     return totalSusceptibles
 
-def getEscapeProbability(outputDir, scenario, seed, numDays):
-    totalSusceptibles = getSusceptiblesAtStart(outputDir, scenario, seed)
-    finalSize = getFinalOutbreakSize(outputDir, scenario, seed, numDays)
-    # susceptibles - infected) / susceptibles
-    escapeProbability = (totalSusceptibles - finalSize) / totalSusceptibles
+def getEscapeProbability(outputDir, scenarioName, seed, finalOutbreakSize):
+    totalSusceptibles = getSusceptiblesAtStart(outputDir, scenarioName, seed)
+    escapeProbability = (totalSusceptibles - finalOutbreakSize) / totalSusceptibles
     return escapeProbability
 
-def escapeProbabilityPlot(outputDir, scenarioNames, scenarioDisplayNames, numDays):
+def escapeProbabilityPlot(outputDir, scenarioNames, scenarioDisplayNames, numDays, extinctionThreshold):
     allEscapeProbabilities = []
     for scenario in scenarioNames:
+        print(scenario)
         seeds = getRngSeeds(outputDir, scenario)
         scenarioEscapeProbabilities = []
         for s in seeds:
-            scenarioEscapeProbabilities.append(getEscapeProbability(outputDir, scenario, s, numDays))
+            print("        " + scenario + str(s))
+            finalOutbreakSize = getFinalOutbreakSize(outputDir, scenario, s, numDays)
+            if finalOutbreakSize > extinctionThreshold:
+                scenarioEscapeProbabilities.append(getEscapeProbability(outputDir, scenario, s, finalOutbreakSize))
         allEscapeProbabilities.append(scenarioEscapeProbabilities)
     plt.boxplot(allEscapeProbabilities, labels=scenarioDisplayNames)
     plt.ylabel("Escape probability")
-    plt.ylim(0, 1.1)
-    plt.show()
+    plt.ylim(0, 1.05)
+    plt.savefig(os.path.join(outputDir, "EscapeProbabilityPlot.png"))
+    plt.clf()
 
 def main(outputDir, numDays, extinctionThreshold):
     scenarioNames = ["Scenario1", "Scenario2", "Scenario3", "Scenario4"]
@@ -103,12 +62,7 @@ def main(outputDir, numDays, extinctionThreshold):
                             "Age-dependent\nimmunity rates\n+ no clustering",
                             "Uniform\nimmunity rates\n+ clustering",
                             "Age-dependent\nimmunity rates +\nclustering"]
-    # Occurrence of outbreaks
-    outbreakOccurrencePlot(outputDir, scenarioNames, scenarioDisplayNames, numDays, extinctionThreshold)
-    # Final size distribution without extinction cases
-    finalSizesPlot(outputDir, scenarioNames, scenarioDisplayNames, numDays, extinctionThreshold)
-    # Escape probabilities
-    escapeProbabilityPlot(outputDir, scenarioNames, scenarioDisplayNames, numDays)
+    escapeProbabilityPlot(outputDir, scenarioNames, scenarioDisplayNames, numDays, extinctionThreshold)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -117,5 +71,3 @@ if __name__=="__main__":
     parser.add_argument("--extinctionThreshold", type=int, default=0, help="Extinction threshold.")
     args = parser.parse_args()
     main(args.outputDir, args.numDays, args.extinctionThreshold)
-
-'''
