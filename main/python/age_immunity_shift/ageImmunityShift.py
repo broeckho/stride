f50c5355a0880cdf25037160b09d9a828e22752b
Elise Kuylen <elise.kuylen@uantwerpen.be>
2018-12-19 17:20:01 +0100

    Script to create age-immunity files for projected years.

--- main/python/age_immunity_shift/ageImmunityShift.py
+++ main/python/age_immunity_shift/ageImmunityShift.py
@@ -3,7 +3,6 @@ import csv
 import multiprocessing
 import os
 import time
-import xml.etree.ElementTree as ET
 
 from pystride.Event import Event, EventType
 from pystride.PyController import PyController
@@ -53,107 +52,53 @@ def generateRngSeeds(numSeeds):
         seeds.append(int.from_bytes(random_data, byteorder='big'))
     return seeds
 
-def writeSeeds(scenarioName, seeds):
-    with open(scenarioName + "Seeds.csv", "a") as csvfile:
+def writeSeeds(year, R0, seeds):
+    fileName = str(year) + "_R0_" + str(R0) + "_seeds.csv"
+    with open(fileName, "w") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(seeds)
 
-def getRngSeeds(scenarioName):
-    seeds = []
-    seedsFile = scenarioName + "Seeds.csv"
-    with open(seedsFile) as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            for s in row:
-                seeds.append(int(s))
-    return seeds
-
-def getImmunityRate(scenarioName, seed):
-    totalPersons = 0
-    susceptiblePersons = 0
-    susceptiblesFile = os.path.join(scenarioName + "_" + str(seed), "susceptibles.csv")
-    with open(susceptiblesFile) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            totalPersons += 1
-            if int(row["susceptible"]):
-                susceptiblePersons += 1
-    immunityRate = 1 - (susceptiblePersons / totalPersons)
-    return immunityRate
-
-def getAvgImmunityRate(scenarioNames, poolSize):
-    allImmunityRates = []
-    print("Calculating average immunity rate of previous simulations...")
-    for scenario in scenarioNames:
-        seeds = getRngSeeds(scenario)
-        with multiprocessing.Pool(processes=poolSize) as pool:
-            scenarioImmunityRates = pool.starmap(getImmunityRate, [(scenario, s) for s in seeds])
-        allImmunityRates += scenarioImmunityRates
-    return sum(allImmunityRates) / len(allImmunityRates)
-
-def runSimulation(scenarioName, R0, seed, extraParams={}):
-    print("Running " + scenarioName + " with R0 " + str(R0))
+def runSimulation(year, R0, seed):
     configFile = os.path.join("config", "ageImmunityShift.xml")
     control = PyController(data_dir="data")
     control.loadRunConfig(configFile)
-    control.runConfig.setParameter("output_prefix", scenarioName + "_" + str(seed))
+    control.runConfig.setParameter("output_prefix", str(year) +
+                                    "_R0_" + str(R0) + "_" + str(seed))
     control.runConfig.setParameter("rng_seed", seed)
     control.runConfig.setParameter("r0", R0)
-    for paramName, paramValue in extraParams.items():
-        control.runConfig.setParameter(paramName, paramValue)
+    control.runConfig.setParameter("immunity_distribution_file",
+                        os.path.join("data", str(year) + "_measles_immunity.xml"))
     control.registerCallback(registerSusceptibles, EventType.AtStart)
     control.registerCallback(trackCases, EventType.Stepped)
     control.control()
     return
 
-def runSimulations(scenarioName, numRuns, R0, poolSize, extraParams={}):
+def runSimulations(numRuns, year, R0, poolSize):
     seeds = generateRngSeeds(numRuns)
-    writeSeeds(scenarioName, seeds)
-    args = [(scenarioName, R0, seeds[i], extraParams) for i in range(numRuns)]
+    writeSeeds(year, R0, seeds)
+    args = [(year, R0, seeds[i]) for i in range(numRuns)]
     with multiprocessing.Pool(processes=poolSize) as pool:
         pool.starmap(runSimulation, args)
 
-def main(numRuns, R0, poolSize):
+def main(numRuns, years, R0s, poolSize):
     start = time.perf_counter()
-    # Run simulations with age-immunity profile TODAY
-    runSimulations("AgeImmunity2013", numRuns, R0, poolSize,
-                    {"immunity_profile": "AgeDependent",
-                    "immunity_distribution_file": "data/measles_age_immunity_2013.xml"})
-    # Calculate immunity rate
-    immunityRate = getAvgImmunityRate(["AgeImmunity2013"], poolSize)
-    print(immunityRate)
-    # Run simulations without age-immunity profile TODAY
-    runSimulations("Uniform2013", numRuns, R0, poolSize,
-                    {"immunity_profile": "Random", "immunity_rate": immunityRate})
-
-    # Run simulations with age-immunity profile IN THE FUTURE
-    runSimulations("AgeImmunity2033", numRuns, R0, poolSize,
-                    {"immunity_profile": "AgeDependent",
-                    "immunity_distribution_file": "data/measles_age_immunity_2033.xml"})
-    # Calculate immunity rate
-    immunityRate = getAvgImmunityRate(["AgeImmunity2033"], poolSize)
-    print(immunityRate)
-    # Run simulations without age-immunity profile IN THE FUTURE
-    runSimulations("Uniform2033", numRuns, R0, poolSize,
-                    {"immunity_profile": "Random", "immunity_rate": immunityRate})
-
-    # Run simulations with age-immunity profile IN THE FUTURE - accounting for waning
-    runSimulations("AgeImmunity2033Waning", numRuns, R0, poolSize,
-                    {"immunity_profile": "AgeDependent",
-                    "immunity_distribution_file": "data/measles_age_immunity_2033_waning.xml"})
-    # Calculate immunity rate
-    immunityRate = getAvgImmunityRate(["AgeImmunity2033Waning"], poolSize)
-    print(immunityRate)
-    # Run simulations without age-immunity profile IN THE FUTURE - accounting for waning
-    runSimulations("Uniform2033Waning", numRuns, R0, poolSize,
-                    {"immunity_profile": "Random", "immunity_rate": immunityRate})
+    for year in years:
+        for R0 in R0s:
+            print("Running simulations for " + str(year) + " with R0 " + str(R0))
+            runSimulations(numRuns, year, R0, poolSize)
     end = time.perf_counter()
-    print("Total time: {}".format(end - start))
+    totalTimeSeconds = end - start
+    totalTimeMinutes = totalTimeSeconds / 60
+    totalTimeHours = totalTimeMinutes / 60
+    print("Total time: {} seconds or {} minutes or {} hours".format(totalTimeSeconds,
+                                                                totalTimeMinutes,
+                                                                totalTimeHours))
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--numRuns", type=int, default=5, help="Number of simulation runs per scenario")
-    parser.add_argument("--R0", type=int, default=12, help="Basic reproduction number for simulated disease")
+    parser.add_argument("--years", type=int, default=[2013, 2020], help="From which projected year the immunity data should be used")
+    parser.add_argument("--R0s", type=int, nargs="+", default=[12, 13], help="Basic reproduction numbers to simulate for")
     parser.add_argument("--poolSize", type=int, default=8, help="Number of workers in pool for multiprocessing")
     args = parser.parse_args()
-    main(args.numRuns, args.R0, args.poolSize)
+    main(args.numRuns, args.years, args.R0s, args.poolSize)
