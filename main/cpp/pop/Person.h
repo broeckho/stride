6db57c99c7ead0e9ab09567ecb0369130d23224e
Jan Broeckhove <Jan.Broeckhove@uantwerpen.be>
2019-02-14 15:47:02 +0100

    Cleanup "beliefs" leftovers.

--- main/cpp/pop/Person.h
+++ main/cpp/pop/Person.h
@@ -24,9 +24,9 @@
 #include "pool/ContactPoolType.h"
 #include "pool/IdSubscriptArray.h"
 
-namespace stride {
+#include <cstddef>
 
-class Belief;
+namespace stride {
 
 /**
  * Store and handle person data.
@@ -36,7 +36,7 @@ class Person
 public:
         /// Default construction (for population vector).
         Person()
-            : m_age(0.0), m_belief(nullptr), m_gender('M'), m_health(), m_id(0), m_is_participant(), m_pool_ids(),
+            : m_age(0.0), m_gender('M'), m_health(), m_id(0), m_is_participant(), m_pool_ids(),
               m_in_pools()
         {
         }
@@ -44,7 +44,7 @@ public:
         /// Constructor: set the person data.
         Person(unsigned int id, double age, unsigned int householdId, unsigned int k12SchoolId, unsigned int collegeId,
                unsigned int workId, unsigned int primaryCommunityId, unsigned int secondaryCommunityId)
-            : m_age(age), m_belief(nullptr), m_gender('M'), m_health(), m_id(id),
+            : m_age(age), m_gender('M'), m_health(), m_id(id),
               m_is_participant(false), m_pool_ids{householdId, k12SchoolId,        collegeId,
                                                   workId,      primaryCommunityId, secondaryCommunityId},
               m_in_pools(true)
@@ -57,17 +57,8 @@ public:
         /// Get the age.
         double GetAge() const { return m_age; }
 
-        /// Return belief info.
-        Belief* GetBelief() { return m_belief; }
-
-        /// Return belief info.
-        const Belief* GetBelief() const { return m_belief; }
-
         /// Get ID of contactpool_type
         unsigned int GetPoolId(const ContactPoolType::Id& poolType) const { return m_pool_ids[poolType]; }
-
-        /// Return person's gender.
-        char GetGender() const { return m_gender; }
 
         /// Return person's health status.
         Health& GetHealth() { return m_health; }
@@ -89,9 +80,6 @@ public:
 
         /// Participate in social contact study and log person details
         void ParticipateInSurvey() { m_is_participant = true; }
-
-        /// Set the beliefs. Pointer into Population's beliefcontainer.
-        void SetBelief(Belief* belief) { m_belief = belief; };
 
         /// Update the health status and presence in contact pools.
         void Update(bool isWorkOff, bool isSchoolOff, bool adaptiveSymptomaticBehavior);
@@ -150,9 +138,8 @@ public:
         }
 
 private:
-        double       m_age;    ///< The age.
-        Belief*      m_belief; ///< Health beliefs related data (raw pointer intentional).
-        char         m_gender;
+        double       m_age;            ///< The age.
+        char         m_gender;         ///< Gender.
         Health       m_health;         ///< Health info for this person.
         unsigned int m_id;             ///< The id.
         bool         m_is_participant; ///< Is participating in the social contact study
