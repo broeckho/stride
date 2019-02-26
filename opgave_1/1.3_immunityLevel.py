c4867407c9657e5d17e239b2a20d865704b255c6
Benjamin-Vandersmissen <benjamin.vandersmissen@student.uantwerpen.be>
2019-02-26 14:20:14 +0100

    fixed 1.3 script

--- opgave_1/1.3_immunityLevel.py
+++ opgave_1/1.3_immunityLevel.py
@@ -1,26 +1,46 @@
 import csv
 import matplotlib.pyplot as plt
 import os
+import random
 
 from pystride.Event import Event, EventType
 from pystride.PyController import PyController
+
+accumulator = []
+
+def outputDirectory(immunity_rate):
+    return "ImmunityLevel_" + str(immunity_rate)
 
 def trackCases(simulator, event):
     """
         Callback function to track cumulative cases
         after each time-step.
     """
-    outputPrefix = simulator.GetConfigValue("run.output_prefix")
-    timestep = event.timestep
+    global accumulator
     cases = simulator.GetPopulation().GetInfectedCount()
-    with open(os.path.join(outputPrefix, "cases.csv"), "a") as csvfile:
+    timestep = event.timestep
+    if len(accumulator) <= timestep:
+        # first simulation
+        accumulator.append(cases)
+    else:
+        accumulator[timestep] += cases
+    
+
+def emptyAccumulator(immunity_rate, nrSimulations):
+    global accumulator
+    
+    cases = [accumulator[i]/nrSimulations for i in range(len(accumulator))]
+    outputPath = outputDirectory(immunity_rate)
+    with open(os.path.join(outputPath, "cases.csv"), "w") as csvfile:
         fieldnames = ["timestep", "cases"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        if timestep == 0:
-            writer.writeheader()
-        writer.writerow({"timestep": timestep, "cases": cases})
+        writer.writeheader()
+        for i in range(len(cases)):
+            writer.writerow({"timestep": i, "cases": cases[i]})
+            
+    accumulator = []
 
-def plotNewCases(outputPrefix, immunityLevels):
+def plotNewCases(immunityLevels):
     """
         Plot new cases per day for a list of vaccination levels.
     """
@@ -28,42 +48,54 @@ def plotNewCases(outputPrefix, immunityLevels):
         days = []
         newCasesPerDay = []
         prevCumulativeCases = 0 # Keep track of how many cases have been recorded until current time-step
-        with open(os.path.join(outputPrefix + "_" + str(v), "cases.csv")) as csvfile:
+        outputPath = outputDirectory(v)
+        with open(os.path.join(outputPath, "cases.csv")) as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 days.append(int(row["timestep"]))
-                cumulativeCases = int(row["cases"])
+                cumulativeCases = float(row["cases"])
                 newCasesPerDay.append(cumulativeCases - prevCumulativeCases)
                 prevCumulativeCases = cumulativeCases
-        plt.plot(days, newCasesPerDay)
+        plt.plot(days, newCasesPerDay, label=str(v)+"% immunity")
         plt.xlabel("Simulation day")
         plt.ylabel("New cases per day")
-        plt.legend(str(v)+"% immunity")
-        plt.show()
+        plt.legend()
+        plt.savefig(outputPath+"/graph.png")
+        plt.clf()
 
-def runSimulation(outputPrefix, immunity_rate):
+def runSimulation(immunity_rate):
     # Set up simulator
     control = PyController(data_dir="data")
     # Load configuration from file
     control.loadRunConfig(os.path.join("config", "outbreak_2019_estimates.xml"))
     # Set some parameters
-    control.runConfig.setParameter("output_prefix", outputPrefix+"_"+str(immunity_rate))
-    control.runConfig.setParameter("immunity_rate", immunity_rate)
+    control.runConfig.setParameter("output_prefix", outputDirectory(immunity_rate))
+    control.runConfig.setParameter("immunity_profile", "Random")
+    control.runConfig.setParameter("immunity_rate", immunity_rate/100)
+    control.runConfig.setParameter("rng_seed", random.randint(0,100000000))
     control.runConfig.setParameter("vaccine_profile", None)
     control.registerCallback(trackCases, EventType.Stepped)
     # Run simulation
     control.control()
 
 def main():
-    outputPrefix = "ImmunityLevel"
-    immunityLevels = [95,96,97,98,99]
+    #Values in percent
+    minImmunityLevel = 70
+    maxImmunityLevel = 71
+    step = 1
+    immunityLevels = list(range(minImmunityLevel, maxImmunityLevel, step))
+
+    nrSimulations = 200
+
+    global accumulator
+
     # Run simulations
     for level in immunityLevels:
-        if (os.path.exists(os.path.join(outputPrefix+"_"+str(level), "cases.csv"))):
-            open(os.path.join(outputPrefix+"_"+str(level), "cases.csv"), 'w').close() #TODO: cleanup
-        runSimulation(outputPrefix, level)
+        for index in range(nrSimulations):
+            runSimulation(level)
+        emptyAccumulator(level, nrSimulations)
     # Post-processing
-    plotNewCases(outputPrefix, immunityLevels)
+    plotNewCases(immunityLevels)
 
 if __name__=="__main__":
     main()
