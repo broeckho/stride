079c7cdcb9b28cc28394da1e2870edcfb3ca3998
Elise Kuylen <elise.kuylen@uantwerpen.be>
2019-03-04 14:49:26 +0100

    Seed immunes instead of susceptibles.

--- main/cpp/disease/Immunizer.h
+++ main/cpp/disease/Immunizer.h
@@ -43,58 +43,55 @@ public:
         template <typename T>
         void Random(T& pools, std::vector<double>& immunityDistribution, double immunityLinkProbability)
         {
-                // Initialize a vector to count the population per age class [0-100].
-                std::vector<double> populationBrackets(100, 0.0);
+        		// Sampler for int in [0, pools.size()) and for double in [0.0, 1.0).
+        		const auto poolsSize = static_cast<int>(pools.size());
+        		auto intGenerator = m_rn_manager[0].variate_generator(trng::uniform_int_dist(0, poolsSize));
+        		auto uniform01Generator = m_rn_manager[0].variate_generator(trng::uniform01_dist<double>());
 
-                // Count individuals per age class and set all "susceptible" individuals "immune".
-                // note: focusing on measles, we expect the number of susceptible individuals
-                // to be less compared to the number of immune.
-                // TODO but this is a generic simulator
-                for (auto& c : pools) {
-                        for (const auto& p : c.GetPool()) {
-                                if (p->GetHealth().IsSusceptible()) {
-                                        p->GetHealth().SetImmune();
-                                        populationBrackets[p->GetAge()]++;
-                                }
-                        }
-                }
+        		// Initialize a vector to count the population per age class [0-100].
+        		std::vector<double> populationBrackets(100, 0.0);
 
-                // Sampler for int in [0, pools.size()) and for double in [0.0, 1.0).
-                const auto poolsSize          = static_cast<int>(pools.size());
-                auto       intGenerator       = m_rn_manager[0].variate_generator(trng::uniform_int_dist(0, poolsSize));
-                auto       uniform01Generator = m_rn_manager[0].variate_generator(trng::uniform01_dist<double>());
+        		// Count individuals per age class
+        		for (auto& c : pools) {
+        			for (const auto& p : c.GetPool()) {
+        					populationBrackets[p->GetAge()]++;
+        			}
+        		}
 
-                // Calculate the number of susceptible individuals per age class.
-                unsigned int numSusceptible = 0;
-                for (unsigned int age = 0; age < 100; age++) {
-                        populationBrackets[age] = floor(populationBrackets[age] * (1 - immunityDistribution[age]));
-                        numSusceptible += static_cast<unsigned int>(populationBrackets[age]);
-                }
+        		// Calculate the number of immune individuals per age class.
+        		unsigned int numImmune = 0;
+        		for (unsigned int age = 0; age < 100; age++) {
+        			populationBrackets[age] = floor(populationBrackets[age] * immunityDistribution[age]);
+        			numImmune += static_cast<unsigned int>(populationBrackets[age]);
+        		}
 
-                // Sample susceptible individuals, until all age-dependent quota are reached.
-                while (numSusceptible > 0) {
-                        // random pool, random order of members
-                        const ContactPool&        p_pool = pools[intGenerator()];
-                        const auto                size   = static_cast<unsigned int>(p_pool.GetSize());
-                        std::vector<unsigned int> indices(size);
-                        iota(indices.begin(), indices.end(), 0U);
-                        m_rn_manager[0].shuffle(indices.begin(), indices.end());
+        		// Immunize susceptible individuals, until all age-dependent quota are reached.
+        		while (numImmune > 0) {
+        			// Select random contact pool
+        			const ContactPool& p_pool = pools[intGenerator()];
+        			const auto size = static_cast<unsigned int>(p_pool.GetSize());
 
-                        // loop over members, in random order
-                        for (unsigned int i_p = 0; i_p < size && numSusceptible > 0; i_p++) {
-                                Person& p = *p_pool.GetMember(indices[i_p]);
-                                // if p is immune and his/her age class has not reached the quota => make susceptible
-                                if (p.GetHealth().IsImmune() && populationBrackets[p.GetAge()] > 0) {
-                                        p.GetHealth().SetSusceptible();
-                                        populationBrackets[p.GetAge()]--;
-                                        numSusceptible--;
-                                }
-                                // random draw to continue in this pool or to sample a new one
-                                if (uniform01Generator() < (1 - immunityLinkProbability)) {
-                                        break;
-                                }
-                        }
-                }
+        			// Random order of indices
+        			std::vector<unsigned int> indices(size);
+        			iota(indices.begin(), indices.end(), 0U);
+        			m_rn_manager[0].shuffle(indices.begin(), indices.end());
+
+        			// Loop over members in random order
+        			for (unsigned int i_p = 0; i_p < size && numImmune > 0; i_p++) {
+        				Person& p = *p_pool.GetMember(indices[i_p]);
+        				// If p is susceptible and age class has not reached quota => make immune
+        				if (p.GetHealth().IsSusceptible() && populationBrackets[p.GetAge()] > 0) {
+        					p.GetHealth().SetImmune();
+        					populationBrackets[p.GetAge()]--;
+        					numImmune--;
+        				}
+        				// Random draw to continue in this pool or sample a new one
+        				if (uniform01Generator() < (1 - immunityLinkProbability) ) {
+        					break;
+        				}
+        			}
+
+        		}
         }
 
         /// Cocoon immunization.
