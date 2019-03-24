21282c4bfc9a06d0bc8e147b03643c7d6273633c
breinbits <larsvanroy@hotmail.com>
2019-03-24 13:39:13 +0100

    edited gui cmake

--- gui/main.cpp
+++ gui/main.cpp
@@ -6,7 +6,8 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:///main.qml")));
+
+    engine.load(QUrl(QStringLiteral("main.qml")));
 
     return app.exec();
 }
