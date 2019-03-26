3cfba0220449dd7aaf7fbc517b9c868418d7498d
Benjamin-Vandersmissen <benjamin.vandersmissen@student.uantwerpen.be>
2019-03-26 21:51:49 +0100

    added contact pool data for daycares and preschools

--- main/cpp/geopop/GeoGridConfig.h
+++ main/cpp/geopop/GeoGridConfig.h
@@ -112,8 +112,18 @@ public:
         // -----------------------------------------------------------------------------------------
         struct
         {
-                /// Every houselhold constitutes a single ContactPool.
+                /// Every household constitutes a single ContactPool.
                 unsigned int pools_per_household = 1U;
+
+                /// Used to calculate the number of DayCares.
+                unsigned int daycare_size      = 29U;
+                unsigned int pools_per_daycare = 1U;
+                unsigned int daycare_pool_size = 29U;
+
+                /// Used to calculate the number of PreSchools.
+                unsigned int preschool_size      = 120U;
+                unsigned int pools_per_preschool = 6U;
+                unsigned int preschool_pool_size = 20U;
 
                 /// Used to calculate the number of K12Schools.
                 unsigned int k12school_size      = 500U;
