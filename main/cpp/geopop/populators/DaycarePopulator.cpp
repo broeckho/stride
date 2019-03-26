7cd4db643321405b665837d6c618870bd1fc4495
Benjamin-Vandersmissen <benjamin.vandersmissen@student.uantwerpen.be>
2019-03-26 20:18:15 +0100

    Added 2 new Contact Types and added a DayCarePopulator

---
+++ main/cpp/geopop/populators/DaycarePopulator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by benji on 26/03/19.
+//
+
+#include "DaycarePopulator.h"
---
+++ main/cpp/geopop/populators/DaycarePopulator.h
@@ -0,0 +1,14 @@
+//
+// Created by benji on 26/03/19.
+//
+
+#ifndef STRIDE_DAYCAREPOPULATOR_H
+#define STRIDE_DAYCAREPOPULATOR_H
+
+
+class DaycarePopulator {
+
+};
+
+
+#endif //STRIDE_DAYCAREPOPULATOR_H
