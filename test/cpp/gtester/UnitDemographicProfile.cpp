26ba185c0cae5a082807cb15077cc935be8ae2ca
larsvanroy <=>
2019-03-28 15:30:11 +0100

    added tests for 1.2.4

---
+++ test/cpp/gtester/UnitDemographicProfile.cpp
@@ -0,0 +1,20 @@
+//
+// Created by lars on 28-3-19.
+//
+
+/**
+ * @file
+ * Unit tests of Demographic Profiles.
+ */
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(DemographicProfile, age){
+    EXPECT_TRUE(false);
+}
+
+TEST(DemographicProfile, cities){
+    EXPECT_TRUE(false);
+}
