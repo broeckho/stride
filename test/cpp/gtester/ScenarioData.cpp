f474a0999651024299ae09c91f6316dbaea65f9d
Jan Broeckhove <Jan.Broeckhove@uantwerpen.be>
2019-05-21 16:52:32 +0200

    Test margin modified.

--- test/cpp/gtester/ScenarioData.cpp
+++ test/cpp/gtester/ScenarioData.cpp
@@ -60,7 +60,7 @@ tuple<ptree, unsigned int, double> ScenarioData::Get(string tag)
             {"r0_8_geopop", 15900U}};
 
         const map<string, double> margins_geopop = {{"influenza_a_geopop", 3.0e-03}, {"influenza_b_geopop", 0.0},
-                                                    {"influenza_c_geopop", 1.0e-1},  {"measles_16_geopop", 8.0e-02},
+                                                    {"influenza_c_geopop", 2.0e-1},  {"measles_16_geopop", 8.0e-02},
                                                     {"measles_26_geopop", 2.0e-03},  {"r0_0_geopop", 1.0e-03},
                                                     {"r0_12_geopop", 5.0e-02},       {"r0_16_geopop", 5.0e-02},
                                                     {"r0_4_geopop", 8.0e-02},        {"r0_8_geopop", 1.0e-01}};
