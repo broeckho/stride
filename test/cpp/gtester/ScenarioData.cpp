87f47071ce45ee513b6b2173d2250f6f8da9de61
Elise Kuylen <elise.kuylen@uantwerpen.be>
2019-08-12 12:53:22 +0200

    Try to fix error with gcc7.

--- test/cpp/gtester/ScenarioData.cpp
+++ test/cpp/gtester/ScenarioData.cpp
@@ -57,7 +57,7 @@ tuple<ptree, unsigned int, double> ScenarioData::Get(string tag)
             {"influenza_a_geopop", 533700U}, {"influenza_b_geopop", 0U},     {"influenza_c_geopop", 5U},
             {"measles_16_geopop", 157500U},  {"measles_26_geopop", 600000U}, {"r0_0_geopop", 1200U},
             {"r0_12_geopop", 22500U},        {"r0_16_geopop", 39700U},       {"r0_4_geopop", 3400U},
-            {"r0_8_geopop", 10100U}};
+            {"r0_8_geopop", 10000U}};
 
         const map<string, double> margins_geopop = {{"influenza_a_geopop", 3.0e-03}, {"influenza_b_geopop", 0.0},
                                                     {"influenza_c_geopop", 2.0e-1},  {"measles_16_geopop", 8.0e-02},
