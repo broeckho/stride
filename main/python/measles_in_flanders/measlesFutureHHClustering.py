68314820bdbf267193a846c9a4c700e647b2d20f
Elise Kuylen <elise.kuylen@uantwerpen.be>
2019-12-18 16:39:51 +0100

    Added script for future HH clustering.

---
+++ main/python/measles_in_flanders/measlesFutureHHClustering.py
@@ -0,0 +1,98 @@
+import argparse
+import multiprocessing
+import numpy
+import os
+import time
+
+from measlesClusteringCallbacks import registerAssociativityCoefficient, registerSusceptibles
+from measlesClusteringSimulations import runSimulation
+from measlesClusteringUtil import generateRngSeeds, writeRngSeeds, getCommonParameters
+
+from pystride.Event import Event, EventType
+
+from postprocessing import AgeImmunity, InfectedByAge, Clustering, EffectiveR
+from postprocessing import EscapeProbability, OutbreakOccurrence, OutbreakSize
+
+
+def runSimulations(numRuns, years, transmissionProbabilities, clusteringLevels, poolSize):
+    start = time.perf_counter()
+
+    callbacks = [
+        (registerAssociativityCoefficient, EventType.AtStart),
+        (registerSusceptibles, EventType.AtStart)
+    ]
+
+    extraParams = {
+        "immunity_link_probability": 0,
+        "immunity_profile": "AgeDependent",
+        "num_days": 730,
+        "track_index_case": "false",
+        "vaccine_profile": "AgeDependent",
+        "vaccine_clustering_pooltype": "Household",
+    }
+
+    for year in years:
+        extraParams["immunity_distribution_file"] = os.path.join("data", str(year) + "measles_adult_immunity.xml")
+        extraParams["vaccine_distribution_file"] = os.path.join("data", str(year) + "_measles_child_immunity.xml")
+        for prob in transmissionProbabilities:
+            extraParams["transmission_probability"] = prob
+            for level in clusteringLevels:
+                extraParams["vaccine_link_probability"] = level
+                outputPrefix = "AGEDEPENDENT_" + str(year) + "_CLUSTERING_" + str(level) + "_TP_" + str(prob)
+                seeds = generateRngSeeds(numRuns)
+                writeRngSeeds(outputPrefix, seeds)
+                with multiprocessing.Pool(processes=poolSize) as pool:
+                    pool.starmap(runSimulation, [(outputPrefix, s, extraParams, callbacks) for s in seeds])
+    print("Simulations took {} seconds".format(time.perf_counter() - start))
+
+def postprocessing(numRuns, years, transmissionProbabilities, clusteringLevels, extinctionThreshold, poolSize):
+    start = time.perf_counter()
+    outputDir = "."
+    scenarioName = "AGEDEPENDENT"
+    numDays = 730
+    for year in years:
+        AgeImmunity.createAgeImmunityOverviewPlot(outputDir, scenarioName + "_" + str(year),
+                                                transmissionProbabilities, clusteringLevels, poolSize,
+                                                targetLevelsDir="../../Workspace/ProjectedImmunity",
+                                                targetLevelsYear=year)
+        Clustering.createAssortativityCoefficientPlot(outputDir, scenarioName + "_" + str(year),
+                                                transmissionProbabilities, clusteringLevels,
+                                                poolSize, ageLim=99)
+        InfectedByAge.meanAgeOfInfectionHeatmap(outputDir, scenarioName + "_" + str(year),
+                                                transmissionProbabilities, clusteringLevels,
+                                                numDays, poolSize)
+
+        EffectiveR.createEffectiveRHeatmap(outputDir, scenarioName + "_" + str(year),
+                                                transmissionProbabilities, clusteringLevels,
+                                                poolSize)
+        EscapeProbability.createEscapeProbabilityHeatmapPlot(outputDir, scenarioName + "_" + str(year),
+                                                transmissionProbabilities, clusteringLevels,
+                                                numDays, poolSize)
+        OutbreakOccurrence.createOutbreakProbabilityHeatmap(outputDir, scenarioName + "_" + str(year),
+                                                transmissionProbabilities, clusteringLevels,
+                                                numDays, extinctionThreshold, poolSize)
+        OutbreakSize.createOutbreakSizes3DPlot(outputDir, scenarioName + "_" + str(year),
+                                                transmissionProbabilities, clusteringLevels,
+                                                numDays, extinctionThreshold, poolSize)
+        for prob in transmissionProbabilities:
+            OutbreakSize.createExtinctionThresholdHistogram(outputDir, scenarioName + "_" + str(year),
+                                                prob, clusteringLevels, numDays, poolSize)
+    print("postprocessing took {} seconds".format(time.perf_counter() - start))
+    
+def main(postprocessingOnly, numRuns, years, extinctionThreshold, poolSize):
+    transmissionProbabilities = numpy.arange(0.35, 0.65, 0.05)
+    clusteringLevels = [0,0.25, 0.5, 0.75, 1]
+    if not postprocessingOnly:
+        runSimulations(numRuns, years, transmissionProbabilities, clusteringLevels, poolSize)
+    postprocessing(numRuns, years, transmissionProbabilities, clusteringLevels, extinctionThreshold, poolSize)
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--postprocessingOnly", action="store_true", default=False)
+    parser.add_argument("--numRuns", type=int, default=5)
+    parser.add_argument("--years", type=int, nargs="+", default=[2020, 2025, 2030, 2035, 2040])
+    parser.add_argument("--extinctionThreshold", type=int, default=0)
+    parser.add_argument("--poolSize", type=int, default=8)
+    # TODO cutoff for natural immunity?
+    args = parser.parse_args()
+    main(args.postprocessingOnly, args.numRuns, args.years, args.extinctionThreshold, args.poolSize)
